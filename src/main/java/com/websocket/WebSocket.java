efaa26d05abdc7c5c3491fce83249cba1fc0c62c
JuanQuinteros04 <juanmanuelquinteros3@gmail.com>
2024-03-26 20:19:19 -0300

    First Commit

---
+++ src/main/java/com/websocket/WebSocket.java
@@ -0,0 +1,11 @@
+package com.websocket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebSocket {
+    public static void main(String[] args) {
+        SpringApplication.run(WebSocket.class);
+    }
+}
